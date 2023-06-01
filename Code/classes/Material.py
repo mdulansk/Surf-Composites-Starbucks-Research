38d9e94feafd8805300f1acd201a96ec0ed7cd23
unknown <mdulansk@ucsd.edu>
2023-05-31 23:03:51 -0700

    updated and improved Test Specimen and Material classes

--- Code/classes/Material.py
+++ Code/classes/Material.py
@@ -1,22 +1,12 @@
+import numpy as np
 class Material:
-    material_properties = {
-    "6oz_Eglass": {"E1": 29.7E9, "E2": 29.7E9, "G12": 5.3E9, "v12": 0.17},
-    "4oz_Eglass": {"E1": 29.7E9 * 2/3, "E2": 29.7E9 * 2/3, "G12": 5.3E9 * 2/3, "v12": .17},
-    "EPS_Foam": {"E1": 3.80E7, "E2": 3.80E7, "G12": 1.9E7, "v12": 0.32},
-    "PU_Foam": {"E1": .220E9, "E2": .220E9, "G12": 0, "v12": 0, "rho": .240 * 1000},
-    "Balsa": {"E1": 2.20E9, "E2": 2.20E9, "G12": .106E9, "v12": .25},
-    "Basswood": {"E1": 7.1E9, "E2": 7.1E9, "G12": 0, "v12": 0, "rho": 320},
-    "Carbon": {"E1": 150E9, "E2": 0, "G12": 0, "v12": 0, "rho": 1600}
-}
 
-    def __init__(self, material_name):
-        if material_name in Material.material_properties:
-            self.material_name = material_name
-        else:
-            raise ValueError("Material '{}' properties has not been recorded.".format(material_name))
-
-    def __getattr__(self, name):
-        if name in Material.material_properties[self.material_name]:
-            return Material.material_properties[self.material_name][name]
-        else:
-            raise AttributeError("'Material' object has no attribute '{}'".format(name))
+    def __init__(self,Name,E1=np.nan,E2=np.nan,G12=np.nan,v12=np.nan,rho=np.nan):
+        self.Name = Name
+        self.E1 = E1
+        self.E2 = E2
+        self.G12 = G12
+        self.v12 = v12
+        self.rho = rho
+        
+    
--- Code/classes/SandwichPanel.py
+++ Code/classes/SandwichPanel.py
@@ -10,6 +10,7 @@ from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 
 class TestSpecimen:
+
     def __init__(self, construction):
         self.construction = construction
         self.E = []
@@ -21,75 +22,68 @@ class TestSpecimen:
     def calculate_E(self):
         mat_thickness = 0  # if multiple materials are given
         num_plies = {}
-        for key in self.construction:
-            material_info = self.construction[key]
-            if isinstance(material_info[0], list):
-                if key == 'topLam':
-                    num_plies['top'] = len(material_info)
+        for loc,material in self.construction.items():
+            if isinstance(material[0], list):
+                if loc == 'Top Lam':
+                    num_plies['top'] = len(material)
                 else:
-                    num_plies['bot'] = len(material_info)
-                for mat_info in material_info:
-                    mat_name = mat_info[0]
-                    if len(material_info) == 1:
-                        self.get_material_properties(mat_name)
+                    num_plies['bot'] = len(material)
+                for mat_info in material:
+                    mat = mat_info[0]
+                    if len(material) == 1:
                         continue
                     mat_thickness = mat_info[1]
-                    self.get_abd_material_properties(mat_name)
-                if len(material_info) == 1:
+                if len(material) == 1:
+                    self.construction[loc] = self.construction[loc][0]
                     continue
-                self.get_abd([y[0] for y in material_info],mat_thickness)
-            else:
-                mat_name = material_info[0]
-                self.get_material_properties(mat_name)
-        self.get_geometry(self.construction['core'][-1],self.construction['core'][-2],\
-            self.construction['Stringer'][1],self.construction['topLam'][0][1],num_plies)
+                self.get_abd(mat_thickness,loc)
+        self.get_geometry(self.construction['Core'][-1],self.construction['Core'][-2],\
+            self.construction['Stringer'][1],self.construction['Top Lam'][-1],num_plies)
         self.calculate_moments_of_inertia()
 
-    def get_material_properties(self, mat_name):
-        material = Material(mat_name)
-        self.materials[mat_name] = material
-        print(mat_name)
-        self.E.append(material.E1)
-    
-    def get_abd_material_properties(self, mat_name):
-        abd_materials = Material(mat_name)
-        self.abd_materials[mat_name] = abd_materials
-        #self.E.append(abd_materials.E1)
-
-    def get_abd(self, abd_mat_names,t):
-        E1 = []
-        E2 = []
-        G12 = []
-        v12 = []
-        for mat_name in abd_mat_names:
-            abd_material = self.abd_materials[mat_name]
-            E1.append(abd_material.E1)
-            E2.append(abd_material.E2)
-            G12.append(abd_material.G12)
-            v12.append(abd_material.v12)
-        theta =  [0 for _ in range(len(E1))]
-        A_top,B_top,D_top = ABD(E1, E2, G12, v12, [0 for i in range(len(E1))], t)
-        a = np.linalg.inv(A_top)
-        E1_top = 1/(a[0,0]*(2*t))
-        print(theta)
-        self.E.append(E1_top)
+    def get_abd(self,t,location):
+        E1s = []
+        E2s = []
+        G12s = []
+        v12s = []
+        rhos = []
+        for laminate in self.construction[location]:
+            E1s.append(laminate[0].E1)
+            E2s.append(laminate[0].E2)
+            G12s.append(laminate[0].G12)
+            v12s.append(laminate[0].v12)
+            rhos.append(laminate[0].rho)
+        A,B,D = ABD(E1s, E2s, G12s, v12s, [0 for _ in range(len(E1s))], t)
+        a = np.linalg.inv(A)
+        E1 = 1/(a[0,0]*(len(E1s)*t))
+        E2 = 1/(a[1,1]*(len(E1s)*t))
+        G12 = 1/(a[2,2]*(len(E1s)*t))
+        v12 = -a[1,0]/a[0,0]
+        rho = sum(rhos)/len(rhos)
+        self.construction[location] = [Material(location,E1,E2,G12,v12,rho),len(E1s)*t]
 
     def get_geometry(self, w_foam,h_foam,w_string,t,num_plies):
             bot_lam = [[0,w_foam,w_foam,0],[0,0,num_plies['bot']*t,num_plies['bot']*t]]
-            top_lam = [[0,w_foam,w_foam,0],[num_plies['bot']*t+h_foam,num_plies['bot']*t+h_foam,\
-                                              num_plies['bot']*t+h_foam+num_plies['top'],num_plies['bot']*t+h_foam+num_plies['top']]]
-            foam_left = [[0, (w_foam/2)-(w_string/2),(w_foam/2)-(w_string/2),0],[num_plies['bot']*t,\
-                                                    num_plies['bot']*t,num_plies['bot']*t+h_foam,num_plies['bot']*t+h_foam]]
+            top_lam = [[0,w_foam,w_foam,0],\
+    [num_plies['bot']*t+h_foam,num_plies['bot']*t+h_foam,num_plies['bot']*t+h_foam+num_plies['top']*t,num_plies['bot']*t+h_foam+num_plies['top']*t]]
+            foam_left = [[0, (w_foam/2)-(w_string/2),(w_foam/2)-(w_string/2),0],\
+                         [num_plies['bot']*t,num_plies['bot']*t,num_plies['bot']*t+h_foam,num_plies['bot']*t+h_foam]]
             string = [[(w_foam/2)-(w_string/2),(w_foam/2)+(w_string/2),(w_foam/2)+(w_string/2),(w_foam/2)-(w_string/2)],\
                       [num_plies['bot']*t,num_plies['bot']*t,num_plies['bot']*t+h_foam,num_plies['bot']*t+h_foam]]
             foam_right = [[(w_foam/2)+(w_string/2),w_foam,w_foam,(w_foam/2)+(w_string/2)],\
                           [num_plies['bot']*t,num_plies['bot']*t,num_plies['bot']*t+h_foam,num_plies['bot']*t+h_foam]]
+            
             self.yzs.append(foam_left)
-            self.yzs.append(string)
             self.yzs.append(foam_right)
+            self.yzs.append(string)
             self.yzs.append(bot_lam)
             self.yzs.append(top_lam)
 
     def calculate_moments_of_inertia(self):
-        # Fix the inputs to moments_of_inertia to fit better with this class
+        self.materials = {'Foam Left':['silver',self.construction['Core'][0].rho],\
+               'Foam Right':['silver',self.construction['Core'][0].rho],\
+                'Stringer':['orange',self.construction['Stringer'][0].rho],\
+                'Bot Lam':['brown',self.construction['Bot Lam'][0].rho],\
+                'Top Lam':['brown',self.construction['Top Lam'][0].rho]}
+        self.E = [self.construction[loc][0].E1 for loc in ['Core','Core','Bot Lam','Top Lam']]
         self.EIyy, self.EIzz, self.EIyz, self.EA, self.weight = moments_of_inertia(np.array(self.yzs), self.E, self.materials)
